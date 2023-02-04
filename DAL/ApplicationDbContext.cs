f6eff63e68282252eb0385735b6eb10c2eebc32b
srplanta <srplanta@DESKTOP-MAE99H0>
2023-02-04 09:14:30 +0500

    Add project files.

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BOL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Add following NuGet packages in DAL project also
+//  1. Microsoft.EntityFrameworkCore
+//  2. Microsoft.EntityFrameworkCore.Design
+//  3. Microsoft.EntityFrameworkCore.SqlServer
+//  4. Microsoft.EntityFrameworkCore.Tools
+namespace DAL
+{
+    public class ApplicationDbContext : DbContext   //DbContext requires Microsoft.EntityFrameworkCore
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"server=DESKTOP-MAE99H0; database=MultiLayerDb; trusted_connection=true;encrypt=false;TrustServerCertificate=true;");
+        }
+
+        //Add reference of each model class from BOL project
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ DAL/CategoryDb.cs
@@ -0,0 +1,45 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CategoryDb
+    {
+        private ApplicationDbContext dbContext = new ApplicationDbContext();
+        public IEnumerable<Category> GetAll()
+        {   // IEnumerable cannot be manipulated by the user
+            // List can be manipulated
+            return dbContext.Category;
+        }
+
+        public Category GetById(int id)
+        {
+            return dbContext.Category.Find(id);
+        }
+
+        public bool Insert(Category category)
+        {
+            dbContext.Category.Add(category);
+            dbContext.SaveChanges();
+            return true;
+        }
+
+        public bool Update(Category category)
+        {
+            dbContext.Category.Update(category);
+            dbContext.SaveChanges();
+            return true;
+        }
+        
+        public bool Delete(int id)
+        {
+            dbContext.Category.Remove(dbContext.Category.Find(id));
+            dbContext.SaveChanges();
+            return true;
+        }
+    }
+}
